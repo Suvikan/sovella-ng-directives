6fdbc9003092f2072124db841d51f5c10d2d86e1
Suvikan <sanledell@gmail.com>
2023-01-18 13:19:15 +0200

    add display-details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DisplayDetailsComponent } from './display-details/display-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayDetailsComponent
   ],
   imports: [
     BrowserModule,
